a27f4ca7ab0c57a8a6dcdbff03dd0b2b508bd5b4
Your name hereKonstantin <your_email@example.comkonstantinschiller@gmail.com>
2023-04-27 07:46:30 -0400

    Added checkUI functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e){
     e.preventDefault();
@@ -23,7 +25,11 @@ function addItem(e){
 
     li.appendChild(button);
     
+    // add li to dom
     itemList.appendChild(li);
+
+    checkUI();
+
     itemInput.value = '';
 }
 
@@ -46,7 +52,10 @@ function createIcon(classes){
 // Remove Items
 function removeItem(e){
     if (e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove();
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
     } 
 }
 
@@ -55,9 +64,24 @@ function clearItems(e){
     while(itemList.firstChild){
         itemList.removeChild(itemList.firstChild);
     }
+    checkUI();
+}
+
+// Remove some UI elements when there is no list
+function checkUI(){
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0){
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
 }
 
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+checkUI();
