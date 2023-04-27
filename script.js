7bd852b7e1eb020319545ba1505c270d7845c469
Your name hereKonstantin <your_email@example.comkonstantinschiller@gmail.com>
2023-04-27 07:34:16 -0400

    Added clear functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e){
     e.preventDefault();
@@ -26,6 +27,7 @@ function addItem(e){
     itemInput.value = '';
 }
 
+// Create Button
 function createButton(classes) {
     const button = document.createElement('button')
     button.className = classes;
@@ -34,10 +36,28 @@ function createButton(classes) {
     return button;
 }
 
+// Create Clear Icon
 function createIcon(classes){
     const icon = document.createElement('i');
     icon.className = classes;
     return icon;
 }
+
+// Remove Items
+function removeItem(e){
+    if (e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove();
+    } 
+}
+
+// Clear List
+function clearItems(e){
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
